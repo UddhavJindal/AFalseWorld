0bd6af798b3b00c669506589f0ea63f8e595012c
Yuvraj Gupta <yg27052002@gmail.com>
2022-05-28 11:30:40 +0530

    Collectible fixed

--- Side Scroller Project/Assets/Scripts/TimeTrial/TTCollectable.cs
+++ Side Scroller Project/Assets/Scripts/TimeTrial/TTCollectable.cs
@@ -5,35 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class TTCollectable : MonoBehaviour
 {
-    [Header("Settings")]
-    bool canPick;
-
-    private void Update()
-    {
-        if (canPick && Input.GetKeyDown(KeyCode.LeftShift))
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+        if (collision.gameObject.tag == "Player")
         {
             GameObject.FindGameObjectWithTag("Portal").GetComponent<TTPortal>().currentCoins++;
             if (GameObject.FindGameObjectWithTag("Portal").GetComponent<TTPortal>().currentCoins == GameObject.FindGameObjectWithTag("Portal").GetComponent<TTPortal>().maxCoins)
             {
                 GameObject.FindGameObjectWithTag("Portal").GetComponent<TTPortal>().portalOpen = true;
             }
-                Destroy(gameObject);
+            Destroy(gameObject);
         }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            canPick = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            canPick = false;
-        }
-    }
+   }
 }
