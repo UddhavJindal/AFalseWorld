fdf2c3f403cf8f9ae9d9c0be3a0eb1befe9dc36c
Yuvraj Gupta <yg27052002@gmail.com>
2022-06-03 16:23:05 +0530

    Npc scene added

--- Side Scroller Project/Assets/Scripts/ULevels/NPC #000001/UCameraTransition.cs
+++ Side Scroller Project/Assets/Scripts/ULevels/NPC #000001/UCameraTransition.cs
@@ -7,27 +7,59 @@ public class UCameraTransition : MonoBehaviour
     [SerializeField] GameObject cameraZoomedOut;
     [SerializeField] GameObject cameraZoomedIn;
 
+    //flag 
+    [SerializeField] bool canStartTransition;
+
+    RootNode rootNode;
+
+    public Node.Status treeStatus = Node.Status.RUNNING;
+
     private void Start()
     {
-        cameraZoomedIn.SetActive(false);
+        rootNode = new RootNode();
+
+        Sequence transitionBehaviour = new Sequence("Behaviour");
+
+        Leaf initialSettings = new Leaf("Initial Settings", InitialSettings);
+        Leaf changeTransition = new Leaf("Initial Settings", ChangingTransition);
+
+        transitionBehaviour.AddChild(initialSettings);
+        transitionBehaviour.AddChild(changeTransition);
+
+        rootNode.AddChild(transitionBehaviour);
+    }
+
+    public Node.Status InitialSettings()
+    {
         cameraZoomedOut.SetActive(true);
+        cameraZoomedIn.SetActive(false);
+        return Node.Status.SUCCESS;
+    }
+
+    public Node.Status ChangingTransition()
+    {
+        if(canStartTransition)
+        {
+            cameraZoomedOut.SetActive(false);
+            cameraZoomedIn.SetActive(true);
+            return Node.Status.SUCCESS;
+        }
+        return Node.Status.RUNNING;
+    }
+
+    private void Update()
+    {
+        if (treeStatus == Node.Status.RUNNING)
+        {
+            treeStatus = rootNode.Process();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            cameraZoomedIn.SetActive(true);
-            cameraZoomedOut.SetActive(false);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            cameraZoomedIn.SetActive(false);
-            cameraZoomedOut.SetActive(true);
+            canStartTransition = true;
         }
     }
 }
